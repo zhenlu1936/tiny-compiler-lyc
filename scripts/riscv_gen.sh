e905d1f62c08d2426810cfdfb517a50b5189a090
zhenlu <zhenlu1936@gmail.com>
2025-05-26 04:46:07 +0000

    hjj's own branch

--- scripts/riscv_gen.sh
+++ scripts/riscv_gen.sh
@@ -1,2 +1,3 @@
-riscv64-unknown-elf-gcc -march=rv32im -mabi=ilp32 -O0 ./test/test.c -S -o ./test/test_t.s -fPIC
-riscv64-unknown-elf-gcc -march=rv32im -mabi=ilp32 -O0 ./test/test.c -o ./test/test.o -fPIC
+riscv-none-elf-gcc -march=rv32im -mabi=ilp32 -O0 ./test/test.c -S -o ./test/test_t.s -fPIC
+riscv-none-elf-gcc -march=rv32im -mabi=ilp32 -O0 ./test/test.c -o ./test/test.o -fPIC
+echo "obj compiled to machine code!"
--- scripts/riscv_my_gen.sh
+++ scripts/riscv_my_gen.sh
@@ -1,2 +1,3 @@
 make test
-riscv64-unknown-elf-gcc -march=rv32im -mabi=ilp32 -O0 ./test/test.s -o ./test/test.o -fPIC
+riscv-none-elf-gcc -march=rv32im -mabi=ilp32 -O0 ./test/test.s -o ./test/test.o -fPIC
+echo "obj compiled to machine code!"
