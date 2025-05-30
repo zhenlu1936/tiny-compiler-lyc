cadd9a506ab25ff9faf6a7f4c06d1351affdad8d
zhenlu <3223364704@qq.com>
2025-05-30 14:20:38 +0800

    完成层次结构?

--- test/test.c
+++ test/test.c
@@ -8,11 +8,17 @@ struct teacher {
 	char *name;
 };
 
-struct classes {
-	int number[30];
+struct cls {
+	int number;
 	struct student students[30];
-	struct student teacher[30];
+	struct teacher teacher[5];
+	struct teacher *advisor;
 };
+
+void init_class(struct cls &clas, int number, struct teacher *advisor) {
+	clas.number = number;
+	clas.advisor = advisor;
+}
 
 void init_student(struct student &s, int number, int score, char *name) {
 	s.number = number;
@@ -21,11 +27,16 @@ void init_student(struct student &s, int number, int score, char *name) {
 }
 
 int main() {
-	struct classes class_1;
-	int i;
+	struct cls classes[10];
+	struct teacher *advisor;
 	char name[20];
-	for (i = 0; i < 30; i = i + 1) {
-		init_student(class_1.students[i], i, 100, name);
+
+	int i, j;
+	for (i = 0; i < 10; i = i + 1) {
+		init_class(classes[i], i, advisor);
+		for (j = 0; j < 30; j = j + 1) {
+			init_student(classes[i].students[j], j, 100, name);
+		}
 	}
 
 	return 0;
